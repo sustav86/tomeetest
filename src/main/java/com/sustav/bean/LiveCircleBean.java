b820457d8ad9db1dd1c1cab22ac3bee9c5ecf891
Anton Sustavov <Anton_Sustavov@epam.com>
2019-07-30 17:35:08 +0300

    add bean livecircle

--- src/main/java/com/sustav/bean/LiveCircleBean.java
+++ src/main/java/com/sustav/bean/LiveCircleBean.java
@@ -1,18 +1,37 @@
 package com.sustav.bean;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
 
-@RequestScoped
-@Named("expressLangBean")
 public class LiveCircleBean {
-    private String s = "Expression Language";
 
-    public String getS() {
-        return s;
+    public LiveCircleBean() {
+        System.out.println(getClass().getName() + " constructor");
     }
 
-    public void setS(String s) {
-        this.s = s;
+    public void job1() {
+        System.out.println("Job 1");
+    }
+
+    public void job2() {
+        System.out.println("Job 2");
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println(getClass().getName() + " post construct");
+    }
+
+    @AroundInvoke
+    public Object around(InvocationContext invocationContext) throws Exception {
+        System.out.println(invocationContext.getMethod() + " invoke around");
+
+        return invocationContext.proceed();
+    }
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println(getClass().getName() + " destroyed");
     }
 }
