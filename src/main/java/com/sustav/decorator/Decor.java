fd9f92d716ba6e00241cb982a72c1cf89a4b8f89
Anton Sustavov <asustavov@unitedthinkers.com>
2019-07-30 23:53:01 +0300

    add @Decorator

--- src/main/java/com/sustav/decorator/Decor.java
+++ src/main/java/com/sustav/decorator/Decor.java
@@ -2,16 +2,20 @@ package com.sustav.decorator;
 
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
-@Decorator()
+@Decorator
 public class Decor implements Parent {
     @Inject
     @Delegate
+    @Any
     private Parent parent;
 
     @Override
     public void print() {
-
+        System.out.println("decorator print after");
+        parent.print();
+        System.out.println("decorator print after");
     }
 }
